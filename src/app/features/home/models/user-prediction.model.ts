87a8368fe07298d8be5e3f6563a5441549cc6832
alicia <alicia.re.sanchez@gmail.com>
2026-08-13 11:38:25 +0200

    feat: adapt the service to actual backend calls

--- src/app/features/home/models/user-prediction.model.ts
+++ src/app/features/home/models/user-prediction.model.ts
@@ -1,3 +1,4 @@
+import { CreateSeasonPredictionRequest } from "../../predictions/models/create-season-prediction-request.model";
 import { SeasonPrediction } from "../../predictions/models/season-prediction.model";
 export type UserRole = 'USER' | 'ADMIN';
 export interface User {
@@ -15,4 +16,12 @@ export interface UserPrediction {
 export interface LoginCredentials {
   userName: string;
   password: string;
+}
+
+export interface UserPredictionResponse {
+  id: number,
+  user: User,
+  seasonPrediction: CreateSeasonPredictionRequest,
+  createdAt: string,
+  updatedAt?: string | null;
 }
--- src/app/features/predictions/service/prediction.service.ts
+++ src/app/features/predictions/service/prediction.service.ts
@@ -1,106 +1,32 @@
-import { computed, inject, Injectable, signal } from '@angular/core';
-import { SeasonPrediction } from '../models/season-prediction.model';
-import { User, UserPrediction } from '../../home/models/user-prediction.model';
-import { Password } from 'primeng/password';
-import { AuthService } from '../../../core/service/auth-service';
+import {inject, Injectable} from '@angular/core';
+import { UserPredictionResponse } from '../../home/models/user-prediction.model';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { CreateSeasonPredictionRequest } from '../models/create-season-prediction-request.model';
 
 @Injectable({
   providedIn: 'root',
 })
 export class PredictionService {
-  private readonly storageKey = 'user-predictions';
-  authService = inject(AuthService)
-  
-
-  private readonly userPredictionsState = signal<UserPrediction[]>(this.loadPredictions());
-
-  readonly userPredictions = this.userPredictionsState.asReadonly();
-
-  readonly currentUserPrediction = computed(() =>{
-    const currentUser = this.authService.currentUser();
-    if (!currentUser) {
-      return undefined;
-    }
-    return this.userPredictionsState().find(
-      prediction => prediction.user.id === currentUser.id
-    ) ?? null
-    }
-  );
+  private readonly http = inject(HttpClient);
+  private readonly apiUrl = 'https://localhost:7106/api/'
 
 
-
-  private loadPredictions(): UserPrediction[] {
-    const savedPredictions = localStorage.getItem(
-      this.storageKey
-    );
-
-    if (!savedPredictions) {
-      return [];
-    }
-
-     try {
-      return JSON.parse(savedPredictions);
-    } catch {
-      localStorage.removeItem(this.storageKey);
-      return [];
-    }
+  private getPredictions(): Observable<UserPredictionResponse[]> {
+    return this.http.get<UserPredictionResponse[]>(this.apiUrl + 'predictions');
+  }
+  getCurrentUserPrediction(): Observable<UserPredictionResponse | null> {
+    return this.http.get<UserPredictionResponse|null>(this.apiUrl + 'predictions/me');
+  }
+  getUserPredictionById(userId:number): Observable<UserPredictionResponse | null>{
+    return this.http.get<UserPredictionResponse|null>(this.apiUrl + 'predictions/user/' + userId)
   }
 
-  private savePredictions(predictions: UserPrediction[]): void {
-    localStorage.setItem(
-      this.storageKey,
-      JSON.stringify(predictions)
-    );
-  }
-  getCurrentUserPrediction(): UserPrediction | null {
-    const currentUser = this.authService.currentUser();
-    if(!currentUser) return null;
-    return (
-      this.userPredictionsState().find(
-        userPrediction => userPrediction.user.id === currentUser.id
-      ) ?? null
-    );
+  saveCurrentPrediction(seasonPrediction: CreateSeasonPredictionRequest): Observable<UserPredictionResponse | null> {
+   return this.http.post<UserPredictionResponse | null>(this.apiUrl + 'predictions', seasonPrediction)
   }
 
-  saveCurrentPrediction(seasonPrediction: SeasonPrediction): UserPrediction {
-    const currentUser = this.authService.currentUser();
-    if (!currentUser) {
-      throw new Error('No se puede guardar una predicción sin iniciar sesión');
-    }
-
-    const existingPrediction = this.getCurrentUserPrediction();
-
-    const userPrediction: UserPrediction = {
-      user: currentUser,
-      seasonPrediction,
-      createdAt:
-        existingPrediction?.createdAt ??
-        new Date().toISOString(),
-      updatedAt: new Date().toISOString()
-    };
-
-    this.userPredictionsState.update(predictions => {
-      let updatedPredictions: UserPrediction[];
-
-      if (!existingPrediction) {
-        updatedPredictions = [
-          ...predictions,
-          userPrediction
-        ];
-      } else {
-        updatedPredictions = predictions.map(
-          prediction =>
-            prediction.user.id === currentUser.id
-              ? userPrediction
-              : prediction
-        );
-      }
-
-    this.savePredictions(updatedPredictions);
-
-    return updatedPredictions;
-  });
-
-  return userPrediction;
+  updateCurrentPrediction(prediction: CreateSeasonPredictionRequest): Observable<UserPredictionResponse> {
+    return this.http.put<UserPredictionResponse>(this.apiUrl, prediction );
   }
 }
