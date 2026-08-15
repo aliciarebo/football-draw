909c6d1b4983bc5246136523a89d1d110842a72e
alicia <alicia.re.sanchez@gmail.com>
2026-08-15 10:42:28 +0200

    feat: added interceptor and calls for logging in and registering

--- src/app/features/login/pages/login-pages-component/login-pages-component.ts
+++ src/app/features/login/pages/login-pages-component/login-pages-component.ts
@@ -1,8 +1,9 @@
 import { Component, inject } from '@angular/core';
 import { LoginFormComponent } from "../../component/login-form-component/login-form-component";
 import { AuthService } from '../../../../core/service/auth-service';
-import { LoginCredentials, User } from '../../../home/models/user-prediction.model';
+import { LoginCredentials, User, UserCreationRequest } from '../../../home/models/user-prediction.model';
 import { Router } from '@angular/router';
+import { AuthFacade } from '../../../../core/facade/auth.facade';
 
 @Component({
   selector: 'app-login-pages-component',
@@ -11,17 +12,17 @@ import { Router } from '@angular/router';
   styleUrl: './login-pages-component.css',
 })
 export class LoginPagesComponent {
-  loginService = inject(AuthService);
+  authFacade = inject(AuthFacade);
   readonly router = inject(Router);
 
   login(user: LoginCredentials){
-    const loginSuccess = this.loginService.login(user);
-
-    if (!loginSuccess) {
-      return;
-    }
+     this.authFacade.login(user);
 
     this.router.navigate(['/myPrediction']);
 
+  }
+
+  register(user: UserCreationRequest){
+    this.authFacade.createUser(user)
   }
 }
